c1a29fd8504488863f7f712008a779bf0c98f313
ArnaudSpanneut <arnaud.spanneut@gmail.com>
2021-02-02 10:37:15 -0500

    refactor(create): add helper services

--- create.js
+++ create.js
@@ -1,9 +1,8 @@
 import * as uuid from "uuid";
-import AWS from "aws-sdk";
+import handler from "./libs/handler-lib";
+import dynamoDb from "./libs/dynamodb-lib";
 
-const dynamoDb = new AWS.DynamoDB.DocumentClient();
-
-export async function main(event, context) {
+export const main = handler(async (event, context) => {
   // Request body is passed in as a JSON encoded string in 'event.body'
   const data = JSON.parse(event.body);
   const { content, attachment } = data;
@@ -22,4 +21,4 @@ export async function main(event, context) {
   await dynamoDb.put(params);
 
   return params.Item;
-}
+});
